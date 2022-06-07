4b6d777a22b24699ee2037958225368c9a95b0c7
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-06-06 22:26:06 -0400

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//we check diretory relative to the file 
+import { App } from './components/App';
+
+//Creates a root element in the DOM
+//React DOM library is used to interact with the DOM
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
